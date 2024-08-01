9f76915da16303e5a35184ca34d1436f7bacc4b0
amb2007 <agustinmancinibobadilla@gmail.com>
2024-08-01 11:17:55 -0300

    act1-finished

--- act1/src/App.jsx
+++ act1/src/App.jsx
@@ -1,16 +1,38 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-function app(){
-  const [products,setProducts] = useState([]);
-  const [text,setText] = useState('');
-  const search = (event) =>{
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [products, setProducts] = useState([]);
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then((response) => response.json())
+      .then((json) => {
+        setProducts(json);
+      });
+  }, []);
+
+  const search = (event) => {
     setText(event.target.value);
-    console.log(products.filter((product) => {
-      product.title.StartWith(text);
-    }))
-  }
-  useEffect(() =>
-  fetch('https://fakestoreapi.com/products')
-  .then(res=>res.json())
-  .then(json=>console.log(json)))
+    
+  };
+
+  const filtro = products.filter((product) =>
+    product.title.toLowerCase().startsWith(text.toLowerCase())
+  );
+  
+
+  return (
+    <>
+      <input type="text" onChange={search} />
+      <ul>
+        {filtro.map((product) => (
+          <li key={product.id}>{product.title}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
+
+export default App;
